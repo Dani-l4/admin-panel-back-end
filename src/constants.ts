49d80d11a931553668e5836c88c8e1bb36ab9402
Mirash <mirash.ru137@mail.ru>
2024-05-04 19:22:28 +0400

    edit cookie settings

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const COOKIE_SETTINGS = {
     REFRESH_TOKEN: {
         httpOnly: true,
         maxAge: 604800000, // 7 days
-        // secure: true // w https
+        secure: true, // w https
     },
 }
 export const ACCESS_TOKEN_EXPIRATION = 1800000 // 30 min
